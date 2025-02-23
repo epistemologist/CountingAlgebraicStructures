27c6bbe20a33a1481f2e3cf640b133e4328443af
epistemologist <husnain.raza2048@gmail.com>
2025-02-22 20:24:09 -0600

    Classified order 6 semigroups!

--- README.md
+++ README.md
@@ -49,3 +49,41 @@ We have that:
 Note that for any set $S$ with $|S| = n$, a closed operation $\cdot: S^2 \to S $ is essentially a function from the set $\underbrace{ \{ (1, 1), \cdots (1, n), \cdots (n,1), \cdots, (n,n)\} }_{n^2 \text{ elements}} \mapsto \{ 1 \cdots n \}$ - there are $n^{n^2}$ such functions.
 
 A subset of these multiplications are associative. We use a similar approach to the [DPLL algorithm](https://en.wikipedia.org/wiki/DPLL_algorithm) to [count](./count_semigroups.py) all non-isomorphic semigroups of small orders:
+
+```python
+   ...: for n in range(1, 7):                                                                                                                 
+   ...:     X = number_of_semigroups(n,  suppress_output=(n<5))                                                                               
+   ...:                                                      
+Counted 1 of order 1 in 0:00:00.001019                                 
+Counted 5 of order 2 in 0:00:00.001440                                 
+Counted 24 of order 3 in 0:00:00.024050                                                                                                       
+Counted 188 of order 4 in 0:00:02                                      
+[+] Generating possible Cayley tables...                               
+319484it [03:44, 1422.24it/s]                                                                                                                 
+[+] Filtering out some isomorphic models                               
+100%|██████████████████████████████████████████████████████████████████████████████████████████████| 115552/115552 [00:05<00:00, 22491.26it/s]
+[+] Final isomorphism check                                                                                                                   
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 1914/1914 [06:13<00:00,  5.13it/s]
+Counted 1915 of order 5 in 0:10:05
+```
+
+```
+Counted 1 semigroups of order 1 in 0:00:00.007976
+Counted 5 semigroups of order 2 in 0:00:00.030633
+Counted 24 semigroups of order 3 in 0:00:00.114370
+Counted 188 semigroups of order 4 in 0:00:01
+[+] Generating possible Cayley tables...
+                             [+] Filtering out some isomorphic models
+319484it [02:01, 2633.90it/s]                                                                                                         | 8692/115552 [00:02<00:32, 3282.46it/s]
+100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 115552/115552 [00:07<00:00, 16426.16it/s]
+[+] Final isomorphism check
+100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1914/1914 [00:07<00:00, 251.59it/s]
+Counted 1915 semigroups of order 5 in 0:02:13
+[+] Generating possible Cayley tables...
+29451749it [6:16:25, 274980.90it/s][+] Filtering out some isomorphic models
+29539010it [6:18:27, 1300.83it/s]  
+100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 9267678/9267678 [24:55<00:00, 6198.24it/s]
+[+] Final isomorphism check███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████▉| 9267122/9267678 [24:55<00:00, 45100.98it/s]
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 28633/28633 [37:23<00:00, 12.76it/s]
+Counted 28634 semigroups of order 6 in 7:18:51
+```
--- count_semigroups.py
+++ count_semigroups.py
@@ -1,12 +1,29 @@
+from collections import deque, Counter
+from concurrent.futures import ProcessPoolExecutor
+from copy import deepcopy
+from hashlib import sha1
+from itertools import cycle, count, islice,  product, permutations
+from math import isqrt
+from multiprocessing import cpu_count
+from random import shuffle
 from typing import *
 from tqdm import tqdm
-from itertools import count, product, permutations
-from collections import deque
-from copy import deepcopy
-
 
 # Represent magma as dict of tuples
 Magma = Dict[Tuple[int,int], int]
+
+# GAP and other software uses tables to represent semigroups and other algebraic structures
+# So we use this method to convert between their representations and ours
+def table_to_magma(table: List[List[int]]) -> Magma:
+    # See https://docs.gap-system.org/doc/ref/chap35.html#X85CD1E7678295CA6
+    N = len(table)
+    magma = dict()
+    for i in range(N):
+        for j in range(N):
+            magma[(i,j)] = table[i][j]
+    return magma
+
+SHA1 = lambda s: sha1(s.encode()).hexdigest()
 
 def propagate(model, length):
     curr_len = len(model)
@@ -53,7 +70,6 @@ class PartialModel:
     def __repr__(self):
         return repr(self.to_table())
 
-
     def check_model(self) -> bool:
         for x,y,z in product(range(self.N), repeat=3):
             # (x.y).z = x.(y.z)
@@ -84,26 +100,55 @@ class PartialModel:
             ))
         return [ model for model in new_models if model.check_model() ]
 
-# Some utilities
-def dict_to_frozenset(d: dict) -> frozenset:
-    return frozenset(
-        (k,v) for k,v in d.items()
-    )
+# Class to represent a complete magma
+# i.e. a complete dict {(x,y): z} of size n^2 for some order n
+class CompleteMagma(PartialModel): 
+                    # Only really subclassing for the repr method
+    def __init__(self, magma: Magma):
+        order = isqrt(len(magma))
+        assert len(magma) == order**2 
+        super().__init__(
+            curr_model = magma,
+            length = order,
+            unassigned_pairs = []
+            )
 
-def frozenset_to_dict(s: frozenset) -> dict:
-    return dict(s)
+    def __hash__(self) -> int:
+       # Note that magma can be represented as a list of integers of length order^2 each in [0..order-1]
+        out = 0
+        for i, (_, val) in enumerate( sorted(self.model.items()) ):
+            out += val * pow(self.N, i)
+        return out
+
+    def __iter__(self):
+        return iter(self.model)
+
+    def __getitem__(self, key):
+        return self.model[key]
+
+    def __setitem__(self, key, item):
+        self.model[key] = item
+    
+    def __len__(self):
+        return len(self.model)
 
 # Generate all equivalent magmas up to permutation of entries
 def gen_equivalent_magmas(magma: Magma, n: int) -> List[Magma]:
     return [
-        { (p[x], p[y]): p[x_op_y]
+        CompleteMagma( { (p[x], p[y]): p[x_op_y]
             for (x,y), x_op_y in magma.items()
-        }
+        })
         for p in permutations(range(n))
     ]
 
+def gen_isomorphism_invariant(S: CompleteMagma) -> Tuple[int]:
+    # See https://iopscience.iop.org/article/10.1088/1757-899X/862/5/052047/pdf
+    # Number of each element in the table
+    return tuple(sorted( Counter( S.model.values() ).values() ))
 
-def are_isomorphic(S1: Magma, S2: Magma, n: int) -> bool:
+def are_isomorphic(S1: CompleteMagma, S2: CompleteMagma, n: int) -> bool:
+    if gen_isomorphism_invariant(S1) != gen_isomorphism_invariant(S2):
+        return False
     def is_isomorphism(S1, S2, perm):
         return all(
             perm[ S1[(a,b)] ]  == S2[(perm[a], perm[b])]
@@ -130,32 +175,28 @@ def gen_all_semigroups(order: int):
 
 def gen_isomorphic_semigroups(order: int):
     print("[+] Generating possible Cayley tables...")
-    semigroups = [dict_to_frozenset(i.model) for i in gen_all_semigroups(order) ]
+    semigroups = [CompleteMagma(i.model) for i in gen_all_semigroups(order) ]
     candidates = []
-    equivalent_semigroups = set()
+    equivalent_semigroups = set() # Store as hashes to reduce memory
     print("[+] Filtering out some isomorphic models")
     for semigroup in tqdm( semigroups ):
-        if semigroup not in equivalent_semigroups:
+        if hash(semigroup) not in equivalent_semigroups:
             candidates.append(semigroup)
-            equivalent_semigroups|= {
-                dict_to_frozenset(m) for m in gen_equivalent_magmas(
-                    frozenset_to_dict(semigroup), order
-                )}
+            equivalent_semigroups |= {
+                    hash(semigroup) 
+                    for semigroup in gen_equivalent_magmas(
+                        semigroup.model, order
+                    )}
     print("[+] Final isomorphism check")
     out = [candidates[0]]
     for i in tqdm(range(1, len(candidates))):
         if not any([are_isomorphic(
-            frozenset_to_dict(j),
-            frozenset_to_dict(candidates[i]),
+            j,
+            candidates[i],
             order
         ) for j in out]):
             out.append(candidates[i])
-    return [PartialModel(
-        curr_model=frozenset_to_dict(i),
-        length=order,
-        unassigned_pairs=None
-        ) for i in out
-    ]
+    return [CompleteMagma(i) for i in out]
 
 def number_of_semigroups(n: int, suppress_output: bool) -> int:
     import os, sys
@@ -168,5 +209,8 @@ def number_of_semigroups(n: int, suppress_output: bool) -> int:
             with redirect_stderr(dev_null) if suppress_output else nullcontext(sys.stderr):
                 semigroups = gen_isomorphic_semigroups(n)
     end = time()
-    print(f"Counted {len(semigroups)} of order {n} in {str(timedelta(seconds=(int( end-start ) if end-start > 1 else end-start)))}")
+    print(f"Counted {len(semigroups)} semigroups of order {n} in {str(timedelta(seconds=(int( end-start ) if end-start > 1 else end-start)))}")
     return len(semigroups)
+
+for i in range(1, 7):
+    number_of_semigroups(i, suppress_output=i<5)
